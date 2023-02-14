28c8702e8a30b94a5d7cfffa97a4845029ba11a1
tank <824431999@qq.com>
2023-02-14 18:34:10 +0800

    tag json 支持

--- gormgen.sh
+++ gormgen.sh
@@ -7,12 +7,13 @@ if [ $1 -eq 1 ]; then
     exit 1
 fi
 }
-#printf "\n build gormgen\n\n"
-#go build main.go
-#printf "\n cp gormgen to gopath/bin\n\n"
-#cp main $GOPATH/bin/gormgen
-printf "\n create and generating file\n\n"
-go run main.go -addr "root:@tcp(localhost:3306)/ginadmin"  -tables "admin" -json true
+go mod tidy
+printf "\n build gormgen\n\n"
+go build main.go
+printf "\n cp gormgen to gopath/bin\n\n"
+cp main $GOPATH/bin/gormgen
+#printf "\n create and generating file\n\n"
+#go run main.go -addr "root:@tcp(localhost:3306)/ginadmin"  -tables "admin" -json true
 #gormgen -addr "root:@tcp(localhost:3306)/ginadmin"  -tables "admin"
 shellExit $?
 
