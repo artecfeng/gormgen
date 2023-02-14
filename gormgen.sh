15a62f725cd8975629290db66e044b6731100d0c
tank <824431999@qq.com>
2023-02-14 17:10:45 +0800

    gormgen

--- gormgen.sh
+++ gormgen.sh
@@ -7,10 +7,10 @@ if [ $1 -eq 1 ]; then
     exit 1
 fi
 }
-printf "\n build gormgen\n\n"
-go build main.go
-printf "\n cp gormgen to gopath/bin\n\n"
-cp main $GOPATH/bin/gormgen
+#printf "\n build gormgen\n\n"
+#go build main.go
+#printf "\n cp gormgen to gopath/bin\n\n"
+#cp main $GOPATH/bin/gormgen
 printf "\n create and generating file\n\n"
 gormgen -addr "root:@tcp(localhost:3306)/ginadmin"  -tables "admin"
 shellExit $?
